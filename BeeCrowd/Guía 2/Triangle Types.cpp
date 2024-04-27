0f55c8f2e7a97b4ab74eaa2ae215d168015df8dc
Frank♥ <98553892+Franklynjsb@users.noreply.github.com>
2024-04-27 16:23:58 -0300

    Add: Ejercicio de triangulos

--- BeeCrowd/Guía 2/Triangle Types.cpp
+++ BeeCrowd/Guía 2/Triangle Types.cpp
@@ -1,38 +1,49 @@
 #include <iostream>
+
 using namespace std;
 
-int main(int argc, char *argv[]) {
-	
-	double A,B,C,a,b,c;
+int main() {
+	float A, B, C;
 	cin >> A >> B >> C;
 	
-	// Ordenar los lados de manera decreciente
+	// Reparto usando el temp para ordenarlos de mayor a menor
+	float temp;
+	
 	if (A < B) {
-		swap(A, B);
+		temp = A;
+		A = B;
+		B = temp;
 	}
 	if (A < C) {
-		swap(A, C);
+		temp = A;
+		A = C;
+		C = temp;
 	}
 	if (B < C) {
-		swap(B, C);
+		temp = B;
+		B = C;
+		C = temp;
 	}
-	if (A>=B+B) {
+	
+	//COMPARO PARA DEFINIR SI NO ES UN TRIANGULO
+	if (A >= B + C) {
 		cout << "NAO FORMA TRIANGULO" << endl;
-	} else {
 		
-		if (A*A == B*B + C*C){
+	//SI LO ÉS SE DEFINE EL TIPO
+	} else {
+		if (A * A == B * B + C * C) {
 			cout << "TRIANGULO RETANGULO" << endl;
 		}
-		if (B*A > B*B + C*C) {
+		if (A * A > B * B + C * C) {
 			cout << "TRIANGULO OBTUSANGULO" << endl;
 		}
-		if(A*A < B*B + C*C){
+		if (A * A < B * B + C * C) {
 			cout << "TRIANGULO ACUTANGULO" << endl;
 		}
-		if (A==B && A==C) {
+		//LUEGO DE DEFINIR EL TIPO SE DEFINE EL SUBTIPO
+		if (A == B && B == C) {
 			cout << "TRIANGULO EQUILATERO" << endl;
-		}
-		if ((A == B && B != C) || (B == C && C != A) || (A == C && C != B)){
+		} else if (A == B || B == C || A == C) {
 			cout << "TRIANGULO ISOSCELES" << endl;
 		}
 	}
