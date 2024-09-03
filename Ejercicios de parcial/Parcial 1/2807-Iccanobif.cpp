385a987121b51a1a39a8f62144dbcdea52ecb0de
Franklynjsb <juegosyutilidades@hotmail.com>
2024-09-03 19:15:15 -0300

    Fix: Correccion del ejercicio de Icanobiff

--- Ejercicios de parcial/Parcial 1/2807-Iccanobif.cpp
+++ Ejercicios de parcial/Parcial 1/2807-Iccanobif.cpp
@@ -2,19 +2,21 @@
 #include <iomanip>
 using namespace std;
 
-int fibonacci(int, int, int);
+int fibonacci(int);
 void iccanobif(int);
 
 int main(int argc, char *argv[]) {
 	int n;
+	
 	cin >> n; 
 	iccanobif(n);
 	cout << endl;
+	
 	return 0;
 }
 
 void iccanobif(int numero) {
-	cout << fibonacci(1, 1, numero);
+	cout << fibonacci(numero);
 	if (numero != 1) {
 		cout << " ";
 		iccanobif(numero - 1); 
@@ -23,11 +25,11 @@ void iccanobif(int numero) {
 }
 
 
-int fibonacci(int anteriorAnterior, int anterior, int n) {
-	if (n == 1 or n == 0) return n;
-	if (n != 2) {
-		int esteValor = anteriorAnterior + anterior;
-		return fibonacci(anterior, esteValor, n - 1);
+int fibonacci(int n) {
+	if (n == 1 or n == 0){
+		return n;
 	}
-	else return anterior;
+	else{
+		return fibonacci(n - 2)+fibonacci(n-1);
+	}
 }
