dbe86bd5c5c5668ba7b6bcdd665845ea81752f4b
Franklynjsb <juegosyutilidades@hotmail.com>
2024-10-28 15:18:28 -0300

    Add: ejercicios de strings

--- omegaUp/Strings/Encontrando anagramas.cpp
+++ omegaUp/Strings/Encontrando anagramas.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 #include <string>
 using namespace std;
-//TERMINAR
 
-bool Anagrama (string palabra1, string palabra2);
-bool vectorOrdenado(string palabra);
-void Swap(char valor1, char valor2);
+bool vectorOrdenado(const string palabra);
 void ordenarVector(string palabra);
+void Swap (char valor1, char valor2);
 
 
 int main(int argc, char *argv[]) {
@@ -15,7 +13,13 @@ int main(int argc, char *argv[]) {
 	
 	cin >> palabra1 >> palabra2;
 	
-	if (Anagrama(palabra1, palabra2)) {
+	ordenarVector(palabra1);
+	ordenarVector(palabra2);
+	cout << "PALABRA1 " << palabra1 << " PALABRA2 " << palabra2;
+	
+	if(palabra1.length() != palabra2.length()) {
+		cout << "No :(" << endl;
+	}else if (palabra1 == palabra2) {
 		cout << "ANAGRAMA!" << endl;
 	} else {
 		cout << "No :(" << endl;
@@ -24,45 +28,29 @@ int main(int argc, char *argv[]) {
 	return 0;
 }
 
-bool Anagrama (string palabra1, string palabra2) {
-	int longitud = palabra1.length();
-	
-	ordenarVector(palabra1);
-	ordenarVector(palabra2);
-	
-	cout << palabra1 << endl;
-	cout << palabra2 << endl;
-	
-	for(int i=0;i<longitud;i++) { 
-		if(palabra1[i] != palabra2[i]) {
-			return false;
-		}
-	}
-	
-	
-	return true;
-	
-}
-
 void ordenarVector(string palabra) {
 	
-	int longitud = palabra.length();
+	int longitud = palabra.length()-1;
 	
-	while (!vectorOrdenado(palabra)) {
-		for(int i=1;i<longitud;i++) { 
-			if (palabra[i-1]>palabra[i]) {
-				Swap (palabra[i-1], palabra[i]);
+	for(int j=0;j<longitud;j++) { 
+		
+		while(!vectorOrdenado(palabra)) {
+			
+			for(int i=0;i<longitud-i;i++) { 
+				if (palabra[i]>palabra[i+1]){
+					Swap(palabra[i], palabra[i+1]);
+				}
 			}
 		}
 	}
 }
 
-bool vectorOrdenado(string palabra) {
+bool vectorOrdenado(const string palabra) {
 
-	int longitud = palabra.length();
+	int longitud = palabra.length()-1;
 	
-	for(int i=1;i<longitud;i++) { 
-		if (palabra[i-1]>palabra[i]) {
+	for(int i=0;i<longitud;i++) {
+		if (palabra[i]>palabra[i+1]) {
 			return false;
 		}
 	}
@@ -70,10 +58,9 @@ bool vectorOrdenado(string palabra) {
 	return true;
 }
 
-void Swap(char valor1, char valor2) {
+void Swap (char valor1, char valor2) {
 	char aux= valor1;
 	valor1=valor2;
 	valor2=aux;
 }
-
 
