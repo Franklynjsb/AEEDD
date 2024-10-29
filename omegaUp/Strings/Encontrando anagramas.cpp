26a631179681b819be4717fcb0450e2f9fe84e08
Franklynjsb <juegosyutilidades@hotmail.com>
2024-10-29 17:47:16 -0300

    Ejercicios de string

--- omegaUp/Strings/Encontrando anagramas.cpp
+++ omegaUp/Strings/Encontrando anagramas.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 bool vectorOrdenado(const string palabra);
-void ordenarVector(string palabra);
-void Swap (char valor1, char valor2);
+void ordenarVector(string &palabra);
+void Swap (char &valor1, char &valor2);
 
 
 int main(int argc, char *argv[]) {
@@ -15,28 +16,27 @@ int main(int argc, char *argv[]) {
 	
 	ordenarVector(palabra1);
 	ordenarVector(palabra2);
-	cout << "PALABRA1 " << palabra1 << " PALABRA2 " << palabra2;
 	
 	if(palabra1.length() != palabra2.length()) {
-		cout << "No :(" << endl;
+		cout << "No :(\n";
 	}else if (palabra1 == palabra2) {
-		cout << "ANAGRAMA!" << endl;
+		cout << "ANAGRAMA!\n";
 	} else {
-		cout << "No :(" << endl;
+		cout << "No :(\n";
 	}
 	
 	return 0;
 }
 
-void ordenarVector(string palabra) {
+void ordenarVector(string &palabra) {
 	
-	int longitud = palabra.length()-1;
+	int longitud = palabra.length();
 	
 	for(int j=0;j<longitud;j++) { 
 		
 		while(!vectorOrdenado(palabra)) {
 			
-			for(int i=0;i<longitud-i;i++) { 
+			for(int i=0;i<longitud-1;i++) { 
 				if (palabra[i]>palabra[i+1]){
 					Swap(palabra[i], palabra[i+1]);
 				}
@@ -58,7 +58,7 @@ bool vectorOrdenado(const string palabra) {
 	return true;
 }
 
-void Swap (char valor1, char valor2) {
+void Swap (char &valor1, char &valor2) {
 	char aux= valor1;
 	valor1=valor2;
 	valor2=aux;
