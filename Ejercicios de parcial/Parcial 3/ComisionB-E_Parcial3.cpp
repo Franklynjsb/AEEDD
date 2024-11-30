91cb2b171718decf9b8cf95a3973046a3e19b135
franco quijada <Franklynjsb>
2024-11-30 00:17:41 -0300

    Add: Ejercicios Parcial3

---
+++ Ejercicios de parcial/Parcial 3/ComisionB-E_Parcial3.cpp
@@ -0,0 +1,101 @@
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+struct Nodo {
+	int num;
+	struct Nodo *sig;
+};
+typedef Nodo * NodoPtr;
+
+void insertar( NodoPtr & sPtr, int valor );
+void mostrarLista( NodoPtr actual );
+void Parcial3( NodoPtr & L1);
+void OrdenaDos(int & n, int & n2);
+bool Ordenadin (NodoPtr L1);
+
+int main() {
+	
+	NodoPtr L1 = NULL;
+	int valor;
+	
+	while ( cin >> valor ){
+		insertar( L1, valor );
+	}
+	
+	Parcial3( L1 );
+	mostrarLista( L1 );
+	
+	
+	return 0;
+}
+void insertar( NodoPtr & sPtr, int valor ) {
+	NodoPtr nuevo;
+	if (sPtr == NULL) {
+		nuevo = new Nodo;
+		if ( nuevo != NULL ) {
+			nuevo->num = valor;
+			nuevo->sig = NULL;
+			sPtr = nuevo;}
+		else cout << "No hay espacio";
+	}
+	else insertar (sPtr->sig, valor);
+}
+
+void Parcial3( NodoPtr & L1){
+	
+	int contador=0;
+	
+	while (!Ordenadin(L1)) {
+		NodoPtr aux = L1;
+		
+		while (aux->sig != NULL) {
+			if(aux->num > aux->sig->num){
+				OrdenaDos(aux->num, aux->sig->num);
+				contador++;
+			}
+			aux=aux->sig;
+		}
+		
+	}
+	
+	if(contador==0){
+		cout<<"Lista inicialmente ordenada"<<endl;
+	}else{
+		
+		NodoPtr nuevo = new Nodo;
+		if(nuevo!=NULL){
+			nuevo->num=(-1)*contador;
+			nuevo->sig=L1;
+			L1=nuevo;
+		}
+	}
+	
+}
+	
+	bool Ordenadin (NodoPtr L1) {
+		
+		NodoPtr aux = L1;
+		
+		while(aux!=NULL && aux->sig!=NULL){
+			if(aux->num > aux->sig->num){
+				return false;
+			}
+			aux=aux->sig;
+		}
+		
+		return true;
+	}
+	void OrdenaDos(int & n, int & n2){
+		int aux=n;
+		n=n2;
+		n2=aux;
+	}
+		
+		void mostrarLista( NodoPtr actual ) {
+			while ( actual != NULL ) {
+				cout << actual->num << " -> ";
+				actual = actual->sig;
+			}
+			cout << "NULL" << endl;
+		}
+		
